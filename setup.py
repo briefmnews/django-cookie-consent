06529b4bd44506bdbfccf02e9685b51f5afdcb61
Thomas <thomas.carbillet@gmail.com>
2021-01-15 13:27:54 +0100

    feat: update setup.py

--- setup.py
+++ setup.py
@@ -2,29 +2,26 @@ from distutils.core import setup
 from setuptools import find_packages
 
 
-version = '0.2.7.dev0'
+version = '1.0'
 
 CLASSIFIERS = [
     'Framework :: Django',
-    'Framework :: Django :: 1.9',
     'Framework :: Django :: 2.1',
     'Framework :: Django :: 2.2',
-    'Framework :: Django :: 3.0',    
     'Intended Audience :: Developers',
     'License :: OSI Approved :: BSD License',
     'Operating System :: OS Independent',
     'Topic :: Software Development',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
 ]
 
 install_requires = [
-    'Django>=1.9',
+    'Django>=2',
     'django-appconf',
+    'django-model-utils',
 ]
 
 
@@ -36,9 +33,9 @@ setup(
     name="django-cookie-consent",
     description="Django cookie consent application",
     version=version,
-    author="Informatika Mihelac",
-    author_email="bmihelac@mihelac.org",
-    url="https://github.com/bmihelac/django-cookie-consent",
+    author="Brief.me",
+    author_email="tech@brief.me",
+    url="https://github.com/briefmnews/django-cookie-consent",
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     install_requires=install_requires,
